a8f193603429ea65ae7b6f05a76052d64376bfbf
tavisca-akhatri <akhatri@tavisca.com>
2019-07-29 15:27:57 +0530

    file reading done with while loop

--- src/streamFileReader.java
+++ src/streamFileReader.java
@@ -6,17 +6,21 @@ public class streamFileReader  //ctrl+alt+m to create a function
 {
     public String fileReader(String fileName) throws FileNotFoundException
     {
-        String result = "";
+        StringBuilder result = new StringBuilder();
         FileInputStream reader = new FileInputStream(fileName);
         try {
-            byte[] buffer = new byte[reader.available()];
-            reader.read(buffer);
-            result = new String(buffer);
+           // byte[] buffer = new byte[reader.available()];  //available checks the stream contents and gives number of characters
+            byte[] buffer = new byte[8];
+            while(reader.read(buffer)>0){
+                result.append(new String(buffer));
+                buffer = new byte[8];
+            }
+            //result = new String(buffer);
             System.out.println();
         } catch (IOException e) {
             System.out.println(e.getMessage());
             e.printStackTrace();
         }
-        return result;
+        return result.toString();
     }
 }
