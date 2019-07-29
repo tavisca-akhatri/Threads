6fcf6ff8bf374f913646158e9eace2693ac37f28
tavisca-akhatri <akhatri@tavisca.com>
2019-07-29 12:36:25 +0530

    multithreading using callable example 2

--- src/CallableExample2.java
+++ src/CallableExample2.java
@@ -1,18 +1,39 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
+import java.util.function.Function;
 
 public class CallableExample2
 {
-    ExecutorService service = Executors.newFixedThreadPool(4);
-    List<Future<String>> list = new ArrayList<>();
-    Callable<String> c = new TestCallable();
-    for(int i = 0 ; i < 1000 ; i++)
-    {
+    public static void main(String args[]) throws ExecutionException, InterruptedException {
+        ExecutorService service = Executors.newFixedThreadPool(10);  //submit has return type future
+        List<Future<String>> list = new ArrayList<Future<String>>(); //return stored in future and then executed safely
+        Callable<String> call = new TestCallable();
+        for(int i = 0 ; i < 1000 ; i++)
+        {
+            Future<String> future = service.submit(call);  //start thread
+            list.add(future);
 
+        }
+
+        //functional interface
+       Function<Future<String>,String> function = (f)-> {
+            String name="";
+            try {
+                name = f.get();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+            return name;
+        };
+        list.stream().map(function).forEach(System.out::println);
+        for(Future<String> f: list) {
+            System.out.println(System.currentTimeMillis()+"::::"+f.get());
+
+        }
+        service.shutdown();
     }
 }
 
